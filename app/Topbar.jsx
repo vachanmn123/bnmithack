5e76b054e30082a866a267eef956f5efcd3ca61e
Vachan MN <vachanmn123@gmail.com>
2024-06-23 20:38:28 +0530

    Fix more BS

--- app/Topbar.jsx
+++ app/Topbar.jsx
@@ -14,7 +14,7 @@ export default function Topbar() {
     return true;
   };
   return (
-    <div className="w-screen md:max-w-screen-sm fixed top-0 left-0 right-0 bg-secondary h-16 items-center justify-center flex px-4 py-5 z-20 transition-all">
+    <div className="w-screen md:max-w-screen-sm fixed top-0 left-0 right-0 bg-secondary h-16 items-center justify-center flex px-4 py-5 z-20 transition-all mx-auto">
       {hasBack() && (
         <IoIosArrowRoundBack
           className="text-white text-4xl absolute left-2 my-auto cursor-pointer transition-all"
